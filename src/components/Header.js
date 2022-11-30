e997a73831add32e07fa29d4e65f5abc08e64c69
SergeyDedikov <serded59@yandex.ru>
2022-11-30 18:17:26 +0500

    feat: create comp Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import "./Header.css";
 import NavBar from "./NavBar";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className="header">
+      <Logo />
       <NavBar />
     </header>
   );
